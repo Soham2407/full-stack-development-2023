137e8074c16d52bb5441495955a31e351f3a24cb
Soham2407 <nesarikarsoham24@gmail.com>
2023-07-08 11:52:09 +0530

    css and js started

---
+++ 3-JAVASCRIPT/02-Objects/1-objects.js
@@ -0,0 +1,11 @@
+const person = {
+  name: "soham",
+  age: 24,
+  action: function () {
+    console.log("he is doing study");
+  },
+};
+
+console.log(person);
+console.log(person.name);
+person.action();
